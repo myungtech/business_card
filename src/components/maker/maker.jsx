7bbc4676f32e5aa6ba5d46dc6b40eb27d3a01fca
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-11 18:28:37 +0900

    cardAddForm component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -55,17 +55,23 @@ const Maker = ({ authService }) => {
                 history.push('/');
             }
         })
-    })
+    });
+
+    const addCard = (card) => {
+        const updated = [...cards, card];
+        setCards(updated);
+    };
+
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor cards={cards} />
+                <Editor cards={cards} addCard={addCard} />
                 <Preview cards={cards} />
             </div>
             <Footer />
         </section>
-    )
+    );
 };
 
 export default Maker;
