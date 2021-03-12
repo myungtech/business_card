fa0111c36336498117b069e3d18faac77dcfcf64
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-12 18:13:02 +0900

    add loading spinner for file input

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,25 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+    const [loading, setLoading] = useState(false);
+
     const inputRef = useRef();
-    const onButtonClick = (event) => {
+    const onButtonClick = event => {
         event.preventDefault();
         inputRef.current.click();
     };
-    const onChange = async (event) => {
-        console.log(event.target.files[0]);
-        const uploaded = await imageUploader.upload(event.target.files[0]);
-        console.log(uploaded);
 
+    const onChange = async event => {
+        setLoading(true);
+        const uploaded = await imageUploader.upload(event.target.files[0]);
+        setLoading(false);
         onFileChange({
             name: uploaded.original_filename,
             url: uploaded.url,
         });
-
     };
+
     return (
-        <div className={styles.contatiner}>
+        <div className={styles.container}>
             <input
                 ref={inputRef}
                 className={styles.input}
@@ -28,13 +30,17 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
                 name="file"
                 onChange={onChange}
             />
-            <button
-                className={styles.button}
-                onClick={onButtonClick}>
-                {name || 'No File'}
-            </button>
+            {!loading && (
+                <button
+                    className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+                    onClick={onButtonClick}
+                >
+                    {name || 'No file'}
+                </button>
+            )}
+            {loading && <div className={styles.loading}></div>}
         </div>
-    )
-}
+    );
+};
 
 export default ImageFileInput;
