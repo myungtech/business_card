2245ac0bbd0807f186062ea26db52679588e4549
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-12 16:35:04 +0900

    handle image uploading

--- src/components/card_add_form/card_add_form.jsx
+++ src/components/card_add_form/card_add_form.jsx
@@ -1,10 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Button from "../button/button";
-import ImageFileInput from "../image_file_input/image_file_input";
 import styles from "./card_add_form.module.css";
 
 // 정보 입력하는 form
-const CardAddForm = ({ onAdd }) => {
+const CardAddForm = ({ FileInput, onAdd }) => {
   const formRef = useRef();
   const nameRef = useRef();
   const companyRef = useRef();
@@ -12,7 +11,16 @@ const CardAddForm = ({ onAdd }) => {
   const titleRef = useRef();
   const emailRef = useRef();
   const messageRef = useRef();
+  const [file, setFile] = useState({
+    fileName: null, fileURL: null
+  });
 
+  const onFileChange = (file) => {
+    setFile({
+      fileName: file.name,
+      fileURL: file.url,
+    })
+  }
   const onSubmit = (event) => {
     event.preventDefault();
     const card = {
@@ -23,12 +31,13 @@ const CardAddForm = ({ onAdd }) => {
       title: titleRef.current.value || '',
       email: emailRef.current.value || '',
       message: messageRef.current.value || '',
-      fileName: '',
-      fileURL: ''
+      fileName: file.fileName || '',
+      fileURL: file.fileURL || '',
     };
 
     // 사용자가 입력해서 제출하고 나면 form이 reset이 되도록
     formRef.current.reset();
+    setFile({ fileName: null, fileURL: null });
     onAdd(card);
   };
 
@@ -75,7 +84,7 @@ const CardAddForm = ({ onAdd }) => {
         placeholder="Message"
       />
       <div className={styles.fileInput}>
-        <ImageFileInput />
+        <FileInput name={file.fileName} onFileChange={onFileChange} />
       </div>
       <Button name="Add" onClick={onSubmit} />
     </form>
